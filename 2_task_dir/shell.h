bff79e3612c59c325f9d411529cfab5500e1d7da
Franklin Obasi <franklinobasy@gmail.com>
2022-06-07 12:42:51 +0100

    betty compliant, code lines reduced, new functions created to reduce code lines, custom _strncpy

--- 2_task_dir/shell.h
+++ 2_task_dir/shell.h
@@ -14,5 +14,13 @@ void prompt(void);
 void tokenifier(char *cmd, ssize_t line_size);
 void read_cmd(char **argv, char *cmd, ssize_t line_size); 
 
+/* utilities */
+int count_tok(char *cmd, char *delim);
+int count_tok_char(char *token);
+void add_to_argv(char **argv, char *token, int i, int n, char *delim);
+
+/* strfunc */
+char *_strncpy(char *dest, const char *src, int n);
+
 
 #endif
