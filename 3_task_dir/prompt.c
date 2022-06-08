0744432d94cc15c7c4fe548148e9e2aab6ff8861
Franklin Obasi <franklinobasy@gmail.com>
2022-06-08 09:13:41 +0100

    Handled EOF in prompt.c

--- 3_task_dir/prompt.c
+++ 3_task_dir/prompt.c
@@ -11,9 +11,23 @@ void prompt(void)
 	size_t len = 0;
 	ssize_t line_size = 0;
 
-	write(1, "#cisfun$ ", 9);
+	while (line_size != EOF)
+	{
+		if (isatty(STDIN_FILENO))
+			write(1, "#cisfun$ ", 9);
 
-	line_size = getline(&line, &len, stdin);
+		line_size = getline(&line, &len, stdin);
 
-	tokenifier(line, line_size - 1);
+		if (line_size == -1)
+			break;
+
+		if (line_size != 1)
+			tokenifier(line, line_size - 1);
+	}
+
+	free(line);
+
+	if (isatty(STDIN_FILENO))
+		write(1, "\n", 1);
+
 }
