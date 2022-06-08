1168b8a1337d47dad18b56018c8a1d84461e6155
Obelem <ibbraidejesse@gmail.com>
2022-06-08 15:50:13 +0100

    added the env built-in in prompt.c

--- 3_task_dir/prompt.c
+++ 3_task_dir/prompt.c
@@ -1,15 +1,19 @@
 #include "shell.h"
 
 /**
- * prompt - get and read input
- *
+ * prompt - get and read input 
  */
+
+/* Built in global variable to help access environment variables */
+extern char **environ;
 
 void prompt(void)
 {
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t line_size = 0;
+	int i, compare;
+	char *env = "env";
 
 	while (line_size != EOF)
 	{
@@ -17,6 +21,16 @@ void prompt(void)
 			write(1, ":) ", 3);
 
 		line_size = getline(&line, &len, stdin);
+
+		/* compares if input is env and prints env variables if true */
+		compare = strncmp(env, line, 3);
+		if (compare == 0)
+		{
+			for (i = 0; environ[i]; i++)
+				printf("%s\n", environ[i]);
+			
+			prompt();
+		}
 
 		if (line_size == -1)
 			break;
