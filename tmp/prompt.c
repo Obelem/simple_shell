104cd192658057ec50f6dcabbfc1510c9c5a3a29
Obelem <ibbraidejesse@gmail.com>
2022-06-15 10:25:11 +0100

    repo update

--- tmp/prompt.c
+++ tmp/prompt.c
@@ -1,19 +1,49 @@
 #include "shell.h"
 
 /**
- * prompt - get and read input
- *
+ * prompt - get and read input 
  */
 
-void prompt(void)
+/**
+ * 1. removed _exit string, this check is performed in tokenifer() and executed
+ * in exit_process() (...see built-ins.c file)
+ * 2. changed return type to int to handle exit status
+ * returns 1 on success and 0 on failure
+ */
+int prompt(void)
 {
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t line_size = 0;
+	int i, compare;
+	char *env = "env";
 
-	write(1, "#cisfun$ ", 9);
+	while (line_size != EOF)
+	{
+		if (isatty(STDIN_FILENO))
+			write(1, ":) ", 3);
 
-	line_size = getline(&line, &len, stdin);
+		line_size = _getline(&line, &len, stdin);
 
-	tokenifier(line, line_size - 1);
+		/* compares if input is env and prints env variables if true */
+		compare = _strncmp(env, line, 3);
+		if (compare == 0)
+		{
+			for (i = 0; environ[i]; i++)
+				printf("%s\n", environ[i]);
+			
+			prompt();
+			return (1);
+		}
+
+		if (line_size == -1)
+			break;
+
+		if (line_size != 1)
+			tokenifier(line, line_size - 1);
+	}
+	if (isatty(STDIN_FILENO))
+		write(1, "\n", 1);
+
+	return (0);
 }
--- tmp/tokenifier.c
+++ tmp/tokenifier.c
@@ -7,7 +7,12 @@
  *
  */
 
-void tokenifier(char *cmd, ssize_t line_size)
+/**
+ * == changes ==
+ * 1. changed return type to int to cater for exit status
+ * 2. returns 1 on failure or the return value from file_path() on success
+ */
+int tokenifier(char *cmd, ssize_t line_size)
 {
 	char delim[] = " ";
 	char *token, *cmd_buf, *cmd_param;
@@ -20,7 +25,7 @@ void tokenifier(char *cmd, ssize_t line_size)
 	cmd_param = malloc(sizeof(char) * line_size);
 
 	if (!cmd_buf || !cmd_param)
-		return;
+		return (1);
 
 	for (n = 0; cmd[n]; )
 		n++;
@@ -32,7 +37,7 @@ void tokenifier(char *cmd, ssize_t line_size)
 	num_of_tokens = count_tok(cmd, delim);
 	argv = malloc(sizeof(char *) * num_of_tokens);
 	if (!argv)
-		return;
+		return (1);
 	free(cmd);
 
 	/* use cmd_buf to get argument vector array & corresponding elements */
@@ -41,10 +46,10 @@ void tokenifier(char *cmd, ssize_t line_size)
 	{
 		num_of_chars = count_tok_char(token);
 		argv[i] = malloc(sizeof(char) * num_of_chars);
-		strcpy(argv[i], token);
+		_strcpy(argv[i], token);
 		token = strtok(NULL, delim);
 	}
 	argv[i] = NULL;
 	free(cmd_buf);
-	read_cmd(argv, cmd_param, line_size);
+	return (file_path(argv, num_of_tokens, cmd_param, line_size));
 }
